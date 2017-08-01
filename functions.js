0ab5f8a3b4c0ca55bbe856fcb5b7ef3ba2b1dbf2
Isinmin <itformylabs@gmail.com>
2017-08-01 15:36:39 +0300

    v4
    + url parser
    + delete obj
    + change log

--- functions.js
+++ functions.js
@@ -13,11 +13,11 @@ function windowSize() {
 }
 //Кликает на первую кнопку
 function autoClickMenu() {
-
+/*
     var allMenuButtons= document.getElementsByTagName("a");
     allMenuButtons[0].className="active";
     allMenuButtons[0].click();
-    
+    */
 }
 function checkMenuButton(x) {
     str=x.id+"Window";
@@ -34,8 +34,65 @@ function checkMenuButton(x) {
     }
 }
 
-//красит выбранную кнопку в темно-синий
-function  changeButtonColor(x) {
+
+function messageLink() {
+    var window=document.getElementsByClassName("textWindow");
+    for(i=0; i<window.length; i++){
+        window[i].display="none";
+    }
+    var messageWindow=document.getElementById("messages");
+    messageWindow.firstElementChild.click();
+}
+function statusLink() {
+    var window=document.getElementsByClassName("textWindow");
+    for(i=0; i<window.length; i++){
+        window[i].display="none";
+    }
+    var messageWindow=document.getElementById("status");
+    messageWindow.firstElementChild.click();
+}
+function searchTypeObj() {
+    var window=document.getElementsByClassName("textWindow");
+    for(i=0; i<window.length; i++){
+        window[i].display="none";
+    }
+    var messageWindow=document.getElementById("typeObjects");
+    messageWindow.firstElementChild.click();
+}
+function modeObj() {
+    var window=document.getElementsByClassName("textWindow");
+    for(i=0; i<window.length; i++){
+        window[i].display="none";
+    }
+    var messageWindow=document.getElementById("modeObjects");
+    messageWindow.firstElementChild.click();
+}
+
+function createUrl(x) {
+    var myUrl = "";
+    myUrl = "?button=" + x.id;
+    history.pushState(null, null, myUrl )
    
-    x.classList.add("active");
+}
+function get_url_var() {
+    var tmp = new Array();
+    var tmp2 = new Array();
+    var param = new Array();
+
+    var get = location.search;
+    if (get != '') {
+        tmp = (get.substr(1)).split('?');
+        for (var i = 0; i < tmp.length; i++) {
+            tmp2 = tmp[i].split('=');
+            var smth=document.getElementById(tmp2[1]);
+            var father=smth.parentNode;
+            var grandFather=father.parentNode;
+            grandFather.className="active";
+           grandFather.click();
+            document.getElementById(tmp2[1]).className="active";
+            document.getElementById(tmp2[1]).click();
+        }
+
+    }
+    else { document.getElementById("changeLog").click(); }
 }
